89bffe75d0b889f20e321d3adf11223f26e9ceac
Terbuh <hubertt.popowicz@gmail.com>
2024-02-10 19:33:11 +0100

    Add more content in process

---
+++ front/src/app/todolist/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div>Tutaj już rób co trzeba. Bedzięmy robić w sensie hihi</div>
+    )
+}
--- front/src/components/process/processesContent/processesContent.js
+++ front/src/components/process/processesContent/processesContent.js
@@ -12,13 +12,51 @@ export default function ProcessesContent() {
                     <Image
                         src={subtract}
                         alt='kolorek'
-                        className={styles.image}
+                        className={styles.image1}
                     />
                     <div className={styles.absolute}>Konsultacje</div>
                     <div className={styles.number}>1.</div>
 
                 </div>
-                <div className={styles.subtractTitle}></div>
+                <div className={styles.subtractTitle}>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                </div>
+            </div>
+            <div className={styles.titleWrapper}>
+                <div className={styles.subtractWrapper}>
+                    <Image
+                        src={subtract}
+                        alt='kolorek'
+                        className={styles.image3}
+                    />
+                    <div className={styles.absolute}>Proces projektowania</div>
+                    <div className={styles.number}>3.</div>
+
+                </div>
+                <div className={styles.subtractTitle}>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                </div>
+            </div>
+            <div className={styles.titleWrapper}>
+                <div className={styles.subtractWrapper}>
+                    <Image
+                        src={subtract}
+                        alt='kolorek'
+                        className={styles.image5}
+                    />
+                    <div className={styles.absolute}>Przekazanie projektu</div>
+                    <div className={styles.number}>5.</div>
+
+                </div>
+                <div className={styles.subtractTitle}>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+                    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                </div>
             </div>
         </div>
     )
